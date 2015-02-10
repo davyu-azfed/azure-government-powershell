7c9fab339f077f9186a81623f564d0140284be06
Andrew Weiss <andrew.weiss@microsoft.com>
2015-02-10 14:00:03 -0500

    module check

--- ConfigureEnvironment.ps1
+++ ConfigureEnvironment.ps1
@@ -1,3 +1,11 @@
-﻿if (!(Get-AzureEnvironment -Name "AzureGovernmentCloud")) {
+﻿if (!(Get-Module -Name Azure)) {
+    if (!(Get-Module -ListAvailable | ? Name -eq "Azure")) {
+        throw "Azure PowerShell Module is not installed"
+    }
+
+    Import-Module -Name "Azure"
+}
+
+if (!(Get-AzureEnvironment -Name "AzureGovernmentCloud")) {
     Add-AzureEnvironment -Name "AzureGovernmentCloud" -PublishSettingsFileUrl "https://manage.windowsazure.us/publishsettings/index" -ServiceEndpoint "https://management.core.usgovcloudapi.net/" -ManagementPortalUrl "https://manage.windowsazure.us/" -ActiveDirectoryEndpoint "https://login.windows.net" -ActiveDirectoryServiceEndpointResourceId "https://management.core.usgovcloudapi.net/" -StorageEndpoint "core.usgovcloudapi.net"
 }
