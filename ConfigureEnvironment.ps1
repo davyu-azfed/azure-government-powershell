bacd0c1d5f63330aef145cb37a3f4a38a663f6f0
Andrew Weiss <andrew.weiss@microsoft.com>
2015-02-10 13:51:22 -0500

    add environment config

---
+++ ConfigureEnvironment.ps1
@@ -0,0 +1,3 @@
+﻿if (!(Get-AzureEnvironment -Name "AzureGovernmentCloud")) {
+    Add-AzureEnvironment -Name "AzureGovernmentCloud" -PublishSettingsFileUrl "https://manage.windowsazure.us/publishsettings/index" -ServiceEndpoint "https://management.core.usgovcloudapi.net/" -ManagementPortalUrl "https://manage.windowsazure.us/" -ActiveDirectoryEndpoint "https://login.windows.net" -ActiveDirectoryServiceEndpointResourceId "https://management.core.usgovcloudapi.net/" -StorageEndpoint "core.usgovcloudapi.net"
+}
