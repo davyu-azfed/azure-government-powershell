d8375ebeb651f125a4b713d3508da37bff9af98e
Andrew Weiss <andrew.weiss@microsoft.com>
2015-02-10 15:05:45 -0500

    write-output statements

--- ConfigureEnvironment.ps1
+++ ConfigureEnvironment.ps1
@@ -3,9 +3,13 @@
         throw "Azure PowerShell Module is not installed"
     }
 
+    Write-Output "`r`nImporting Azure PowerShell Module"
     Import-Module -Name "Azure"
 }
 
 if (!(Get-AzureEnvironment -Name "AzureGovernmentCloud")) {
+    Write-Output "`r`nAdding AzureGovernmentCloud Environment"
     Add-AzureEnvironment -Name "AzureGovernmentCloud" -PublishSettingsFileUrl "https://manage.windowsazure.us/publishsettings/index" -ServiceEndpoint "https://management.core.usgovcloudapi.net/" -ManagementPortalUrl "https://manage.windowsazure.us/" -ActiveDirectoryEndpoint "https://login.windows.net" -ActiveDirectoryServiceEndpointResourceId "https://management.core.usgovcloudapi.net/" -StorageEndpoint "core.usgovcloudapi.net"
 }
+
+Write-Output "`r`nAzureGovernmentCloud Environment successfully added`r`n"
